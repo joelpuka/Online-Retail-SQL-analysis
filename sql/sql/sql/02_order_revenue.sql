b52ce0df4b9646e9f77bb0cf785711174d1ea690
joelpuka <joelpuka2003@gmail.com>
2026-01-15 10:36:35 -0500

    Delete sql/sql/sql directory

--- sql/sql/sql/02_order_revenue.sql
+++
@@ -1,8 +0,0 @@
--- Aggregate line-item revenue to order level (one row per order)
-CREATE TABLE online_retail_transactions.order_revenue AS
-SELECT
-  InvoiceNo,
-  Country,
-  SUM(revenue) AS order_revenue
-FROM online_retail_transactions.retail_clean_positive
-GROUP BY InvoiceNo, Country;
